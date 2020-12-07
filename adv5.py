034a4f6f3a610a5a075cf53d7eefa49e2679f841
Ajda Remškar <aremskar@Ajdas-MacBook-Pro.local>
2020-12-07 23:08:51 +0100

    Day 6

--- adv5.py
+++ adv5.py
@@ -1,6 +1,34 @@
-import re
+import math
 
 with open('day5.txt', 'r') as f:
     arr_str = [line.strip() for line in f]
 
 n = len(arr_str)
+
+# Part 1
+row=[]
+col=[]
+for i in range(n):
+    row.append(0)
+    col.append(0)
+    for j in range(7):
+        if arr_str[i][j]=='B':
+            row[i]+=math.pow(2,6-j)
+    for j in range(3):
+        if arr_str[i][j+7]=='R':
+            col[i]+=math.pow(2,2-j)
+
+seat_IDs = []
+for i in range(n):
+    seat_IDs.append(row[i]*8+col[i])
+
+print(max(seat_IDs))
+
+# Part 2
+seat_IDs.sort()
+
+i = seat_IDs[0]
+while i in seat_IDs:
+    i+=1
+print(i)
+
---
+++ adv6.py
@@ -0,0 +1,52 @@
+with open('day6.txt', 'r') as f:
+    arr_str = [line.strip() for line in f]
+
+n = len(arr_str)
+
+# Part 1
+groups = ['']
+j = 0
+i = 0
+while i<n:
+    if arr_str[i] == '':
+        groups[j] = sorted(groups[j])
+        groups.append(arr_str[i])
+        j+=1
+    else:
+        groups[j] = groups[j]+arr_str[i]
+    i+=1
+
+qs = 0
+for i in range(len(groups)):
+    qs += len(set(groups[i]))
+
+print(qs)
+
+# Part 2
+groups = [[]]
+j = 0
+i = 0
+while i<n:
+    if arr_str[i] == '':
+        groups.append([])
+        j+=1
+    else:
+        groups[j].append(arr_str[i])
+    i+=1
+
+def check_letter(letter,str_array):
+    for i in str_array:
+        if not (letter in i):
+            return 0
+    return 1
+
+m = len(groups)
+qs = 0
+for i in range(m):
+    for j in groups[i][0]:
+        qs += check_letter(j,groups[i][1:])
+
+print(qs)
+
+
+
